f6a6b7291b6c5d50182e05bafd0b3e53d627935f
None None <hanaperez1101@gmail.com>
2024-11-25 07:15:27 +0100

    new added

--- config/app.php
+++ config/app.php
@@ -187,6 +187,7 @@ return [
 
   'aliases' => Facade::defaultAliases()->merge([
     // 'Example' => App\Facades\Example::class,
+       'Alert' => RealRashid\SweetAlert\Facades\Alert::class,
     'Helper' => App\Helpers\Helpers::class,
   ])->toArray(),
 
